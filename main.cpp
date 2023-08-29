406863a748521548d4c32e5dee6233fe243c0051
Alexandra Vachon <alvachon@student.42quebec.com>
2023-08-29 14:31:52 -0400

    Message set up

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ void irc(int port, int pass)
     //structure for sockets
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in cli_addr;
+	//message message_buffer
+	char message_buffer[256];
 
 
     //create a socket : Doc -> man ip (7)
@@ -29,17 +31,17 @@ void irc(int port, int pass)
 		std::cout << "Socket() is OK!" << std::endl;
 
 	//Bind procedure (clear & setup)
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;//bind call
-    serv_addr.sin_addr.s_addr = INADDR_ANY;//host ip adress
-    serv_addr.sin_port = htons(port);//conversion to network byte order (Ip adress)
-	if (bind(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-	{
-		std::cerr << " Error at bind(); " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
-	else
-		std::cout << "Bind() is OK!" << std::endl;
+   	bzero((char *) &serv_addr, sizeof(serv_addr));
+       serv_addr.sin_family = AF_INET;//bind call
+       serv_addr.sin_addr.s_addr = INADDR_ANY;//host ip adress
+       serv_addr.sin_port = htons(port);//conversion to network byte order (Ip adress)
+   	if (bind(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+   	{
+   		std::cerr << " Error at bind(); " << std::strerror(errno) << std::endl;
+   		exit(1);
+   	}
+   	else
+   		std::cout << "Bind() is OK!" << std::endl;
 
 	//Listen with a backlog queue of 8.
 	listen(socket_fd, 8);
@@ -69,15 +71,19 @@ void irc(int port, int pass)
 	else
 		std::cout << "send() is OK!" << std::endl;
 
-	//setting up message buffer
-	char buffer[256];
-	bzero(buffer, 256);
-
-    int n = read(new_sock_fd, buffer, 255);
+	//setting up reception message_buffer
+	std::cout << "\nRECEPTING ..." << std::endl;
+	bzero(message_buffer, 256);
+	int n = recv(new_sock_fd, message_buffer, 255, 0);
     if (n < 0)
-		throw "Socket reading failure";
+   	{
+   		std::cerr << " Error at recv(); " << std::strerror(errno) << std::endl;
+   		exit(1);
+   	}
+   	else
+   		std::cout << "recv() is OK!" << std::endl;
 
-    std::cout << "here is the message: " << buffer << std::endl;
+    std::cout << "\nMESSAGE:\t" << message_buffer << std::endl;
 
 	n = fcntl(new_sock_fd, F_SETFL, "I got your message\n", O_NONBLOCK); //?
     // n = write(new_sock_fd,"I got your message", 18); // use fcntl() instead of write()
