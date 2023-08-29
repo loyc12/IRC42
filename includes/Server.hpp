d195becac13babf896d4385b6ac1e8020984ef8d
Valerie <zoou2foo@gmail.com>
2023-08-29 09:19:38 -0400

    just starting up on few files and folders

---
+++ includes/Server.hpp
@@ -0,0 +1,15 @@
+/*We can rename the file if not really what we want*/
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+
+#include <iostream>
+#include <stdlib.h> //for which functions??
+#include <fcntl.h>//for file descriptor
+#include<arpa/inet.h> //inet_ntoa
+#include<netinet/in.h>//socket
+#include<sys/socket.h>//socket what is the difference between the two?
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "../includes/Server.hpp" //add proper includes
+
+int	main(int ac, char **av)
+{
+	try
+	{
+		if (ac != 3)
+			throw "Invalid argument count";
+
+		//arg parsing
+		irc(atoi(av[1]), atoi(av[2]));
+	}
+	/*catch (std::string &msg)	//exceptions are char const * for now
+	{
+		std::cerr << "ERROR : " << msg << std::endl;
+		exit(EXIT_FAILURE);
+	}*/
+	catch (char const *msg)
+	{
+		std::cerr << "ERROR : " << msg << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
