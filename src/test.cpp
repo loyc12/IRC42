8bace96241f7ab39b23489dc47145e7f592bd860
Loïc Lord <lordloyc@gmail.com>
2023-08-30 16:03:04 -0400

    wtf

--- src/test.cpp
+++ src/test.cpp
@@ -18,9 +18,9 @@ static void	stop(int sig)
 	//switch our global to stop the infinite loop
 	(void)sig;
 	stopFlag = true;
-	close(socket_fd);
-	close(new_sock_fd);
-	std::cout << "and clean..." << std::endl;
+//	close(socket_fd);
+//	close(new_sock_fd);
+	std::cout << std::endl << "and clean..." << std::endl;
 }
 
 void irc(int port, int pass)
@@ -36,11 +36,20 @@ void irc(int port, int pass)
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd < 0)
 	{
-		std::cerr << " Error at socket();" << std::strerror(errno) << std::endl;
+		std::cerr << " > Error at socket(): " << std::strerror(errno) << std::endl;
 		exit(1);
 	}
 	else
 		std::cout << "Socket() is OK!" << std::endl;
+
+	//makes it so that read/write call to sockets return -1 if blocking
+	if (fcntl(socket_fd, F_SETFL, O_NONBLOCK))
+	{
+		std::cerr << " > Error at fcntl() (socket_fd): " << std::strerror(errno) << std::endl;
+		exit(1);
+	}
+	else
+		std::cout << "fcntl() (socket_fd) is OK!" << std::endl;
 
 	//Bind procedure (clear & setup)
 	bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -49,55 +58,56 @@ void irc(int port, int pass)
     serv_addr.sin_port = htons(port);//conversion to network byte order (Ip adress)
 	if (bind(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 	{
-		std::cerr << " Error at bind(); " << std::strerror(errno) << std::endl;
+		std::cerr << " > Error at bind(): " << std::strerror(errno) << std::endl;
 		exit(1);
 	}
 	else
 		std::cout << "Bind() is OK!" << std::endl;
 
 	//Listen with a backlog queue of 8.
+
 	listen(socket_fd, 8);
 
 	//Accept
-	while (!stopFlag) //change to singleton/global
+	while (!stopFlag) //change to global
 	{
 //		signal(SIGINT, handler_in_loop);
 //		signal(SIGQUIT, handler_in_loop); //good place to put or not?
 		socklen_t	cli_len = sizeof(cli_addr);
 		new_sock_fd = accept(socket_fd, (struct sockaddr *) &cli_addr, &cli_len);
-		if (new_sock_fd < 0)
+		if (new_sock_fd < -1)
 		{
-			std::cerr << " Error at accept();" << std::strerror(errno) << std::endl;
-			exit(1);
+			std::cerr << " > Error at accept(): " << std::strerror(errno) << std::endl;
+			break;
 		}
-		else
-			std::cout << "Accept() is OK!" << std::endl;
+		else if (new_sock_fd != -1)
+		{
+			//Print Connection
+			std::cout << "\nCONNECTED\nCLIENT FROM NETWORK :\t" << inet_ntoa(cli_addr.sin_addr) << "\nNET-TO-HOST PORT :\t" << ntohs(cli_addr.sin_port) << std::endl;
+			std::cout << "\nSERVER IP (local): \t" << inet_ntoa(serv_addr.sin_addr) << "\nHOST-TO-NET PORT :\t" << ntohs(serv_addr.sin_port) << std::endl;
 
-		//Print Connection
-		std::cout << "\nCONNECTED\nCLIENT FROM NETWORK :\t" << inet_ntoa(cli_addr.sin_addr) << "\nNET-TO-HOST PORT :\t" << ntohs(cli_addr.sin_port) << std::endl;
-		std::cout << "\nSERVER IP (local): \t" << inet_ntoa(serv_addr.sin_addr) << "\nHOST-TO-NET PORT :\t" << ntohs(serv_addr.sin_port) << std::endl;
+//			TODO : RECEIVE THE MESSAGE (read ?)
+			char buff[256];
+			bzero(buff, 256);
+			read(new_sock_fd, buff, 255);
+			std::cout << "Message : " << buff << std::endl;
+		}
 
-		std::cout << "\nSENDING ..." << std::endl;
 		// This send() function sends the 14 bytes of the string to the new socket
-		if ((send(new_sock_fd, "Hello, client!\n", 14, 0)) < 0)
+		if ((send(new_sock_fd, "Hello, client!\n", 14, 0)) < -1)
 		{
-			std::cerr << " Error at send();" << std::strerror(errno) << std::endl;
-			exit(1);
+			std::cerr << " > Error at send(): " << std::strerror(errno) << std::endl;
+			break;
 		}
-		else
-			std::cout << "send() is OK!" << std::endl;
+		else if (new_sock_fd != -1)
+		{
+			std::cout << "\nSENDING MESSAGE ..." << std::endl;
 
-		//setting up message buffer
-		char buffer[256];
-		bzero(buffer, 256);
-
-		int n = read(new_sock_fd, buffer, 255);//why 255? not 256? should we use strlen of buffer?
-		if (n < 0)
-			throw "Socket reading failure";
-
-		std::cout << "here is the message: " << buffer << std::endl;
+//			TODO : SEND THE MESSAGE (write ?)
+			write(new_sock_fd, "pong\n", 5);
+		}
 	}
-	// std::cout << "and clean..." << std::endl;
+//	std::cout << "and clean..." << std::endl;
 	close (socket_fd);
 	close (new_sock_fd);
 }
