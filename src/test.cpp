244cb18b5f2cace86dd658630ee22caa62f4c152
Loïc Lord <lordloyc@gmail.com>
2023-08-31 13:12:47 -0400

    manually merged throw()s

--- src/test.cpp
+++ src/test.cpp
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 #include <unistd.h>
-#include <fcntl.h>//file descriptor
+#include <fcntl.h> //		file descriptor
 
-#include <arpa/inet.h>//inet_ntoa
-#include <netinet/in.h>//socket
-#include <sys/socket.h>//socket
+#include <arpa/inet.h> //	inet_ntoa
+#include <netinet/in.h> //	socket
+#include <sys/socket.h> //	socket
 #include "IRC.hpp"
 
-	int new_socket_fd;//deplacer à l'exterieur juste pour voir si stop fonctionne
-	int base_socket_fd;
+int new_socket_fd; //	placer ceci dans serv
+int base_socket_fd;
 
 static void	stop(int sig)
 {
-	//switch our global to stop the infinite loop
+//	Switchs our global bool to stop the infinite loop
 	(void)sig;
 	stopFlag = true;
 	std::cout << "\n > Closing and cleaning ..." << std::endl;
@@ -24,32 +24,26 @@ void irc(int port, int pass)
 {
 	(void) pass;
 
-    // Data for sockets and shit
+//	Data for sockets and shit
 	struct sockaddr_in	server_addr;
 	struct sockaddr_in	client_addr;
 	socklen_t			client_len = sizeof(client_addr);
 	char 				buff[256]; //	FOR MESSAGE RECEIVING/SENDING
 
-    // Create a socket : Doc -> man ip (7)
+//	Create a socket : Doc -> man ip (7)
 	base_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (base_socket_fd < 0)
-	{
-		std::cerr << " > Error at socket(): " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
+		throw std::invalid_argument(" > Error at socket(): ");
 	else
 		std::cout << "Socket() is OK!" << std::endl;
 
-	// Makes it so that read/write call to sockets return -1 if blocking
+//	Makes it so that read/write call to sockets return -1 if blocking
 	if (fcntl(base_socket_fd, F_SETFL, O_NONBLOCK))
-	{
-		std::cerr << " > Error at fcntl() : " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
+		throw std::invalid_argument(" > Error at fcntl() (socket_fd): ");
 	else
 		std::cout << "fcntl() is OK!" << std::endl;
 
-	// Bind procedure (clear & setup)
+//	Bind procedure (clear & setup)
 	bzero((char *) &server_addr, sizeof(server_addr));
 
     server_addr.sin_family = AF_INET; //			bind call
@@ -57,17 +51,16 @@ void irc(int port, int pass)
     server_addr.sin_port = htons(port); //			conversion to network byte order (Ip adress)
 
 	if (bind(base_socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-	{
-		std::cerr << " > Error at bind(): " << std::strerror(errno) << std::endl;
-		exit(1);
-	}
+		throw std::invalid_argument(" > Error at bind(): ");
 	else
 		std::cout << "Bind() is OK!" << std::endl;
 
-	// Listen with a backlog queue of N.
+//	Listen with a backlog queue of N.
 	listen(base_socket_fd, 16);
 
 /*
+//	EXAMPLE CODE FOR MAIN WHILE LOOP GOAL
+
 	while (!stopFlag)
 	{
 		if (conectionCount <= 0)
@@ -93,15 +86,11 @@ void irc(int port, int pass)
 //		Accepts an incoming connection request
 		new_socket_fd = accept(base_socket_fd, (struct sockaddr *) &client_addr, &client_len);
 		if (new_socket_fd < -1)
-		{
-			std::cerr << " > Error at accept(): " << std::strerror(errno) << std::endl;
-			break;
-		}
+			throw std::invalid_argument(" > Error at accept(): ");
 		else if (new_socket_fd != -1)
 		{
 			std::cout << "\nCONNECTED\nCLIENT FROM NETWORK :\t" << inet_ntoa(client_addr.sin_addr) << "\nNET-TO-HOST PORT :\t" << ntohs(client_addr.sin_port) << std::endl;
 			std::cout << "\nSERVER IP (local): \t" << inet_ntoa(server_addr.sin_addr) << "\nHOST-TO-NET PORT :\t" << ntohs(server_addr.sin_port) << std::endl;
-
 		}
 
 //		Receives an incoming message (in theory at least)
@@ -124,19 +113,25 @@ void irc(int port, int pass)
 
 int	main(int ac, char **av)
 {
-	signal(SIGQUIT, SIG_IGN); // reset signal
+	signal(SIGQUIT, SIG_IGN); //	resets signal
 	signal(SIGINT, stop);
 	try
 	{
 		if (ac != 3)
-			throw "Invalid argument count";
-
-		// arg parsing
+			throw std::invalid_argument(" > Error main(): Invalid argument count.");
+//		Arg parsing
 		irc(atoi(av[1]), atoi(av[2]));
 	}
-	catch (char const *msg)
+	catch (std::exception &e)
 	{
-		std::cerr << "ERROR : " << msg << std::endl;
+		std::cerr << e.what() << std::endl;
+		if (errno)
+			std::cout << std::strerror(errno) << std::endl;
+		// if (fdFlag)
+		// {
+		// 	close (socket_fd);
+		// 	close (new_sock_fd);
+		// }
 		exit(EXIT_FAILURE);
 	}
 	exit(EXIT_SUCCESS);
