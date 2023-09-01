1b6a6cfb29cad53e2ca293d8db3eee3a9ded84aa
Loïc Lord <lordloyc@gmail.com>
2023-09-01 10:49:10 -0400

    minor

--- src/server.cpp
+++ src/server.cpp
@@ -9,33 +9,29 @@ Server::Server() : _port(6667), _newSocketFd(0), _baseSocketFd(0) {
 Server::Server(int port) : _port(port), _newSocketFd(0), _baseSocketFd(0) {
 	std::cout << ": Called parameterized constructor (SERVER) " << std::endl;
 }
-
 Server::Server(const Server &other)
 {
 	std::cout << ": Called copy constructor (SERVER) ";
 	this->_port = other.getPort();
 }
-
 Server &Server::operator= (const Server &other)
 {
 	std::cout << ": Called assignment operator (SERVER) ";
 	this->_port = other.getPort();
 	return *this;
 }
-
 Server::~Server()
 {
 	std::cout << ": Called destructor (SERVER) ";
 	//TODO : FREE
 }
-
 std::ostream &operator<< (std::ostream &out, const Server &rhs)
 {
 	out << "IRC Server port : " << rhs.getPort();
 	return (out);
 }
 
-// 0================ OTHER FUNCTIONS ================0
+// 0================ Getters & Setters ================0
 
 const int & Server::getPort(void) const { return (this->_port);}
 const int & Server::getBaseSocket(void) const { return (this->_baseSocketFd);}
