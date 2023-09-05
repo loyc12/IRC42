b1bdd082d4f55bb1087e975d28b48604f7da6b7c
Alexandra Vachon <alvachon@student.42quebec.com>
2023-09-05 10:19:48 -0400

    wrapping select

--- src/test.cpp
+++ src/test.cpp
@@ -17,7 +17,7 @@ void irc(Server *server)
 	struct sockaddr_in	server_addr;
 	struct sockaddr_in	client_addr;
 	socklen_t 			client_len = sizeof(client_addr);
-	char 				buff[BUFFSIZE];
+	//char 				buff[BUFFSIZE];
 
 //	Inits base socket
 	baseSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,65 +26,112 @@ void irc(Server *server)
 	else
 		std::cout << "socket() is OK!" << std::endl;
 
-//	?
+//	bind
 	bzero((char *) &server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET; //					bind call
     server_addr.sin_port = htons(server->getPort()); //		conversion to network byte order (Ip adress)
     server_addr.sin_addr.s_addr = INADDR_ANY; //			host ip adress **INADDR_ANY go get localhost
-
-//	?
 	if (bind(baseSocket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
 		throw std::invalid_argument(" > Error at bind(): ");
 	else
 		std::cout << "bind() is OK!" << std::endl;
 
-//	?
+//	listen
 	listen(baseSocket, SOMAXCONN);
 
-//	( )================ LOYC'S VERSION ================( )
-
-//	Makes read/write type fonctions non-blocking
-/*
-	if (fcntl(baseSocket, F_SETFL, O_NONBLOCK))
-		throw std::invalid_argument(" > Error at fntl(): ");
-	else
-		std::cout << "fcntl() is OK!" << std::endl;
-*/
+	//select
 	fd_set	fdsMaster;
+	int retval;
 	FD_ZERO(&fdsMaster);
 	FD_SET(baseSocket, &fdsMaster);
 
-	std::cout << std::endl << "Awaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port);
+	std::cout << "\nAwaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port)<< std::endl;
 
 //	Client interaction loop
 	while (!stopFlag)
 	{
-		fd_set fdsCopy = fdsMaster;
-		if (select(baseSocket + 1, &fdsCopy, nullptr, nullptr, nullptr) > 0)
-			newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len);
-		if (newSocket > 0)
-		{
-			std::cout << std::endl << "newSocket : " << newSocket << std::endl; //		DEBUG
-			std::cout << std::endl << "Client connected !" << std::endl << std::endl;
-
-//			Receives any potential message from client
-			bzero(buff, BUFFSIZE);
-			int byteReceived = recv(newSocket, buff, BUFFSIZE - 1, MSG_DONTWAIT);
-			if (byteReceived == -1 && errno != EAGAIN) //			is EAGAIN check allowed ??? else must use select()
-				throw std::invalid_argument(" > Error at recv : ");
-			else if (byteReceived == 0)
+			fd_set fdsCopy = fdsMaster;
+			retval = select(baseSocket + 1, &fdsCopy, nullptr, nullptr, nullptr);
+			if (retval == -1)
+				throw std::invalid_argument(" > Error at select(): ");
+			else if (retval)
 			{
-				std::cout<< std::endl << "Client disconnected ..." << std::endl << std::endl;
-				close(newSocket);
-				newSocket = 0;
-				std::cout << std::endl << "Awaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port);
+				std::cout << "select() is OK!" << std::endl;
+				std::cout << "\tListening socket is ready.\n\tWait for FD_ISSET to judge if the fd is ready to be read without blocking." << std::endl;
+				if (FD_ISSET(baseSocket, &fdsCopy))//judge if fd is availlable
+				{
+					std::cout << "FD_ISSET() is OK!" << std::endl;
+					std::cout << "\tThis FD can be read without blocking." << std::endl;
+					newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len);
+					if (newSocket == -1)
+						throw std::invalid_argument(" > Error at accept(): ");
+					else if (newSocket)
+					{
+						std::cout << "accept() is OK!" << std::endl;
+						std::cout << "\t Listened socket returned a new socket to represent the new connection." << std::endl;
+						std::cout << "newSocket : " << newSocket << std::endl;
+						std::cout << std::endl << "Client connected !" << std::endl << std::endl;
+			// //			Receives any potential message from client
+			// 			bzero(buff, BUFFSIZE);
+			// 			int byteReceived = recv(newSocket, buff, BUFFSIZE - 1, MSG_DONTWAIT);
+			// 			if (byteReceived == -1 && errno != EAGAIN) //			is EAGAIN check allowed ??? else must use select()
+			// 				throw std::invalid_argument(" > Error at recv : ");
+			// 			else if (byteReceived == 0)
+			// 			{
+			// 				std::cout<< std::endl << "Client disconnected ..." << std::endl << std::endl;
+			// 				close(newSocket);
+			// 				newSocket = 0;
+			// 				std::cout << std::endl << "Awaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port);
+			// 			}
+			// 			else if (byteReceived > 0)
+			// 				usleep(10000);
+					}
+				}
 			}
-			else if (byteReceived > 0)
-				std::cout << std::string(buff, 1, byteReceived) << std::endl;
-		}
-		else
-			usleep(10000);
 	}
+	close(baseSocket);
+	close(newSocket);
+}
+
+int	main(int ac, char **av)
+{
+	signal(SIGQUIT, SIG_IGN); //ignore ctrl-backslash
+	signal(SIGINT, stop);
+	try
+	{
+		if (ac != 3)
+			throw std::invalid_argument(" > Error main(): Invalid argument count.");
+
+		std::string firstArg = av[1];
+		std::size_t found = firstArg.find_first_not_of("1234567890");
+
+		if (found != std::string::npos)
+			throw std::invalid_argument(" > Error main(): Not a port");
+
+		int port = atoi(av[1]);
+		if (port < 1025 || 65535 < port)
+			throw std::invalid_argument(" > Error main(): Invalid port");
+
+		int pass = atoi(av[2]);
+		(void) pass;
+
+		Server server(port);
+		std::cout << std::endl; //	DEBUG
+
+		irc(&server);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << std::endl << e.what();
+		if (errno)
+			std::cout << std::strerror(errno) << std::endl;
+		close (baseSocket);
+		close (newSocket);
+		std::cout << std::endl << std::endl; //	DEBUG
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
 
 /*
 //	( )======== SELECT TEST (LOYC) ========( )
@@ -176,46 +223,3 @@ void irc(Server *server)
 	}
 
 */
-	close(baseSocket);
-	close(newSocket);
-}
-
-int	main(int ac, char **av)
-{
-	signal(SIGQUIT, SIG_IGN); //ignore ctrl-backslash
-	signal(SIGINT, stop);
-	try
-	{
-		if (ac != 3)
-			throw std::invalid_argument(" > Error main(): Invalid argument count.");
-
-		std::string firstArg = av[1];
-		std::size_t found = firstArg.find_first_not_of("1234567890");
-
-		if (found != std::string::npos)
-			throw std::invalid_argument(" > Error main(): Not a port");
-
-		int port = atoi(av[1]);
-		if (port < 1025 || 65535 < port)
-			throw std::invalid_argument(" > Error main(): Invalid port");
-
-		int pass = atoi(av[2]);
-		(void) pass;
-
-		Server server(port);
-		std::cout << std::endl; //	DEBUG
-
-		irc(&server);
-	}
-	catch (std::exception &e)
-	{
-		std::cerr << std::endl << e.what();
-		if (errno)
-			std::cout << std::strerror(errno) << std::endl;
-		close (baseSocket);
-		close (newSocket);
-		std::cout << std::endl << std::endl; //	DEBUG
-		exit(EXIT_FAILURE);
-	}
-	exit(EXIT_SUCCESS);
-}
