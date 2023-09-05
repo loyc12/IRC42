7da38f1d7f8fc7296f1ea76319f7b762fbe3310a
Loïc Lord <lordloyc@gmail.com>
2023-09-05 11:49:38 -0400

    bugs fixed and bugs created (ctrl-c when connected to client)

--- src/test.cpp
+++ src/test.cpp
@@ -23,7 +23,7 @@ void irc(Server *server)
 	if (baseSocket < 0)
 		throw std::invalid_argument(" > Error at socket(): ");
 	else
-		std::cout << "socket() is OK!" << std::endl;
+		std::cout << "socket() :   OK!" << std::endl;
 
 //	bind
 	bzero((char *) &server_addr, sizeof(server_addr));
@@ -33,7 +33,7 @@ void irc(Server *server)
 	if (bind(baseSocket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
 		throw std::invalid_argument(" > Error at bind(): ");
 	else
-		std::cout << "bind() is OK!" << std::endl;
+		std::cout << "bind() :     OK!" << std::endl;
 
 //	listen
 	listen(baseSocket, SOMAXCONN);
@@ -48,44 +48,50 @@ void irc(Server *server)
 //	Client interaction loop
 	while (!stopFlag)
 	{
-		fd_set fdsCopy = fdsMaster;
-		socketCount = select(baseSocket + 1, &fdsCopy, nullptr, nullptr, nullptr);
+		fd_set fdsRead = fdsMaster;
+//		fd_set fdsWrite = fdsMaster; // for eventual reading; third argument of select()
+
+		// still blocking ...
+		socketCount = select(baseSocket + 1, &fdsRead, nullptr, nullptr, nullptr);
+
 		if (socketCount == -1)
 			throw std::invalid_argument(" > Error at select(): ");
 		else if (socketCount)
 		{
-			std::cout << "\nselect() is OK!" << std::endl;
-			std::cout << "\tListening socket is ready.\n\tWait for FD_ISSET to judge if the fd is ready to be read without blocking." << std::endl;
-			if (FD_ISSET(baseSocket, &fdsCopy))//judge if fd is availlable
+			std::cout << "select() :   OK!" << std::endl; //	Listening socket is ready ...
+
+            if (FD_ISSET(baseSocket, &fdsRead)) //  judges if a fd is ready for interactions (aka; won't block)
 			{
-				std::cout << "\nFD_ISSET() is OK!" << std::endl;
-				std::cout << "\tThis FD can be read without blocking." << std::endl;
+				std::cout << "FD_ISSET() : OK!" << std::endl;
+
 				newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len);
-				if (newSocket == -1)
+
+                if (newSocket < 0)
 					throw std::invalid_argument(" > Error at accept(): ");
 				else if (newSocket)
 				{
-					std::cout << "\naccept() is OK!" << std::endl;
-					std::cout << "\t Listened socket returned a new socket to represent the new connection." << std::endl;
-					std::cout << "\tnewSocket : " << newSocket << std::endl;
-					std::cout << std::endl << "CLIENT CONNECTED!" << std::endl << std::endl;
+					std::cout << "accept() :   OK!" << std::endl;
+					std::cout << "    new socket fd : " << newSocket << std::endl; // gets a new socket fd to represent the every new (re)connections
+
+					std::cout << std::endl << "0========== CLIENT CONNECTED =========0" << std::endl << std::endl;
+
 					//			Receives any potential message from client
 					while (!stopFlag)
 					{
 						bzero(buff, BUFFSIZE);
-						int byteReceived = recv(newSocket, buff, BUFFSIZE - 1, 0);
-						if (byteReceived < 0 && errno != EAGAIN)
+						int byteCount = recv(newSocket, buff, BUFFSIZE - 1, MSG_DONTWAIT);
+						if (byteCount < 0 && errno != EAGAIN)
 							throw std::invalid_argument(" > Error at recv : ");
-						else if (byteReceived == 0)
+						else if (byteCount == 0)
 						{
-							std::cout<< std::endl << "CLIENT DISCONNECTED" << std::endl << std::endl;
-							//close(newSocket);
+							std::cout<< std::endl << "0======== CLIENT DISCONNECTED ========0" << std::endl << std::endl;
+							close(newSocket); //  buggy ?
 							newSocket = 0;
-							std::cout << std::endl << "Awaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port);
-						}
-						else if (byteReceived)
+                            break;
+                        }
+						else if (byteCount)
 						{
-							std::cout << std::string(buff, 0, byteReceived) << std::endl;
+							std::cout << std::string(buff, 0, byteCount);
 						}
 					}
 				}
@@ -97,37 +103,6 @@ void irc(Server *server)
 	close(baseSocket);
 	close(newSocket);
 }
-
-//	( )================ OLD VERSION ================( )
-
-/*
-//	Waits for a client connection
-	std::cout << std::endl << "Awaiting request from client at : " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
-	newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len); //	is blocking
-	std::cout << "Client connected !" << std::endl << std::endl;
-	close(baseSocket);
-
-	char 	buff[BUFFSIZE];
-
-	// Client interaction loop
-	while (stopFlag == false)
-	{
-		bzero(buff, BUFFSIZE);
-		int byteReceived = recv(newSocket, buff, BUFFSIZE - 1, 0); //	is blocking
-		if (byteReceived == -1)
-			throw std::invalid_argument(" > Error at recv :");
-		if (byteReceived == 0)
-		{
-			std::cout << std::endl << "Client disconnected ..." << std::endl << std::endl;
-			break ;
-		}
-        std::cout <<  std::string(buff, 0, byteReceived);
-	}
-	close(baseSocket);
-	close(newSocket);
-}
-*/
-
 
 int main(int ac, char **av)
 {
@@ -152,7 +127,7 @@ int main(int ac, char **av)
     }
     catch (std::exception &e)
     {
-        std::cerr << std::endl << e.what();
+        std::cerr << std::endl << std::endl << e.what();
         if (errno)
             std::cout << std::strerror(errno) << std::endl;
         close (baseSocket);
