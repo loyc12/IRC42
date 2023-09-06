79f207af94bb8501552cf1f38840f68fe6f118f5
Alexandra Vachon <alvachon@student.42quebec.com>
2023-09-06 12:16:35 -0400

    Cleaning

--- src/test.cpp
+++ src/test.cpp
@@ -13,10 +13,9 @@ static void	stop(int sig)
 }
 
 
-int read_from_client(int fd)
+int read_from_client(int fd, std::string *message)
 {
-	char 	buff[BUFFSIZE];
-
+	char 		buff[BUFFSIZE];
 	bzero(buff, BUFFSIZE);
 	int byteReceived = recv(fd, buff, BUFFSIZE - 1, 0);
 	if (byteReceived < 0 && errno != EAGAIN)
@@ -28,13 +27,17 @@ int read_from_client(int fd)
 	}
 	else if (byteReceived)
 	{
-		std::cout << std::string(buff, 0, byteReceived) << std::endl;
+		message->assign(buff, 0, byteReceived);
+		std::cout << *message << std::endl;
 	}
 	return (0);
 }
 
 void irc(Server *server)
 {
+	fd_set				fdsMaster;
+	int 				socketCount;
+	std::string 		message;
 	struct sockaddr_in	server_addr;
 	struct sockaddr_in	client_addr;
 	socklen_t 			client_len = sizeof(client_addr);
@@ -43,9 +46,6 @@ void irc(Server *server)
 	baseSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (baseSocket < 0)
 		throw std::invalid_argument(" > Error at socket(): ");
-	else
-		std::cout << "socket() is OK!" << std::endl;
-
 //	bind
 	bzero((char *) &server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET; //					bind call
@@ -53,63 +53,42 @@ void irc(Server *server)
     server_addr.sin_addr.s_addr = INADDR_ANY; //			host ip adress **INADDR_ANY go get localhost
 	if (bind(baseSocket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
 		throw std::invalid_argument(" > Error at bind(): ");
-	else
-		std::cout << "bind() is OK!" << std::endl;
-
-//	listen
 	listen(baseSocket, SOMAXCONN);
-
-	//select
-	//! fdsMaster OK
-	fd_set	fdsMaster;
-	int 	socketCount;
-	//! FD_ZERO OK
+//	prepare fds for select
 	FD_ZERO(&fdsMaster);
-	//! FD_SET OK
 	FD_SET(baseSocket, &fdsMaster);
 
 //	Client interaction loop
 	while (!stopFlag)
 	{
-		//! fdsCopy OK
 		fd_set fdsCopy = fdsMaster;
-		//^ Lui, il met zero (0) a la place, de base socket ?
 		socketCount = select(FD_SETSIZE, &fdsCopy, nullptr, nullptr, nullptr);
 		if (socketCount == -1)
 			throw std::invalid_argument(" > Error at select(): ");
-		else if (socketCount)
+		else if (socketCount) { for (int i = 0; i < FD_SETSIZE; ++i) { if (FD_ISSET(i, &fdsCopy))
 		{
-			std::cout << "\nselect() is OK!" << std::endl;
-			for (int i = 0; i < FD_SETSIZE; ++i)
+			if (i == baseSocket) /*Connection request on original socket*/
 			{
-				if (FD_ISSET(i, &fdsCopy))//judge if fd is availlable
+				newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len);
+				if (newSocket <= 0)
+					throw std::invalid_argument(" > Error at accept(): ");
+				else
 				{
-					std::cout << "\nFD_ISSET() is OK!" << std::endl;
-					if (i == baseSocket) /*Connection request on original socket*/
-					{
-						newSocket = accept(baseSocket, (struct sockaddr *) &client_addr, &client_len);
-						if (newSocket <= 0)
-							throw std::invalid_argument(" > Error at accept(): ");
-						else
-						{
-							std::cout << "\naccept() is OK!" << std::endl;
-							std::cout << "\tnewSocket : " << newSocket << std::endl;
-							std::cout << std::endl << "NEW CLIENT CONNECTED -> "<< inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl << std::endl;
-							FD_SET(newSocket, &fdsMaster);
-						}
-					}
-					else
-					{
-						if (read_from_client(i) < 0)
-						{
-							close(i);
-							FD_CLR(i, &fdsMaster);
-						}
-					}
-
+					std::cout << "NEW CLIENT ON SOCKET:" << newSocket
+					<< " " << inet_ntoa(client_addr.sin_addr)
+					<< ":" << ntohs(client_addr.sin_port) << std::endl << std::endl;
+					FD_SET(newSocket, &fdsMaster);
 				}
-    		}
-		}
+			}
+			else /*Read message on the known client*/
+			{
+				if (read_from_client(i, &message) < 0)
+				{
+					close(i);
+					FD_CLR(i, &fdsMaster);
+				}
+			}
+		}}}
 	}
 	close(baseSocket);
 	close(newSocket);
