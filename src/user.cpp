1cd1509066d524a2e2991a6e5387aa70330adde6
Alexandra Vachon <alvachon@student.42quebec.com>
2023-09-29 13:27:27 -0400

    pong

--- src/user.cpp
+++ src/user.cpp
@@ -18,13 +18,11 @@ sockaddr_in	User::getClientAddr(void) const 					{ return (this->_client_addr); 
 
 
 
-
-
 // 0================ OTHER FUNCTIONS ================0
 
 void	User::parseUserInfo(std::string *args)
 {
-	std::cout << "here\n" << std::endl;
+	std::cout << "User:parseuserInfo();" << std::endl;
 	this->setUsername(args[1]);
 	this->setMode(args[2]);
 	this->setHostname(inet_ntoa(this->_client_addr.sin_addr));
